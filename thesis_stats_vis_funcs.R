a338cc8def8d37ccf25806f3e2ee8bf896f58ec3
Sampo Vesanen <31888573+sampoves@users.noreply.github.com>
2019-10-31 20:02:09 +0200

    sigTable function

--- thesis_stats_vis_funcs.R
+++ thesis_stats_vis_funcs.R
@@ -124,7 +124,7 @@ GetANOVA <- function(thisFormula, response, explanatory, inputdata,
   #brownf <- bf.test(thisFormula, data = inputdata[-columnsToRemove])
   
   
-  
+
   #### Printing of results ####
   
   # Desc
@@ -160,4 +160,60 @@ GetANOVA <- function(thisFormula, response, explanatory, inputdata,
   cat("# Robust Tests of Equality of Means #")
   cat("\n-------------------------------------\n")
   bf.test(thisFormula, data = inputdata[-columnsToRemove])
+}
+
+
+
+SigTableToShiny <- function(sigTable, hasHeading) {
+  
+  # Use this function to show significance tables in Shiny. It will be useful
+  # with Levene and ANOVA results. The main functionality here is to include
+  # the elusive signifinance star.
+  
+  # Levene test dataframe requires transposing. Levene table
+  # has an attribute heading while ANOVA doesn't. Use this.
+  if (is.null(attributes(sigTable)$heading)) {
+    # ANOVA
+    res <- as.data.frame(do.call(rbind, sigTable))
+  } else {
+    # Levene
+    res <- t(as.data.frame(do.call(rbind, sigTable)))
+  }
+  
+  # Take into account that the table may have an attribute heading. Ask user
+  # if this is the case
+  if (hasHeading == FALSE){
+    sigTablePosition = 2
+  } else {
+    sigTablePosition = 3
+  }
+  
+  # Get the location of the signif.star
+  signif_ncol <- ncol(read.table(textConnection(
+    capture.output(sigTable)[sigTablePosition]), 
+    fill = TRUE))
+  
+  # get signif.star
+  signif_star <- read.table(textConnection(
+    capture.output(sigTable)[sigTablePosition]), 
+    fill = TRUE)[[signif_ncol]]
+  
+  # repeated_na takes into account that the significance table may have more
+  # rows than two.
+  repeated_na <- rep("NA", nrow(res) - 1)
+  signif_star <- c(as.character(signif_star), repeated_na)
+  
+  # bind column signif_star to result.
+  res <- cbind.data.frame(res, signif_star)
+  
+  # Name rows. Try to detect differences in Levene and ANOVA summary tables.
+  if(is.null(rownames(sigTable[[1]]))){
+    # Levene
+    rownames(res) <- rownames(sigTable)
+  } else {
+    # ANOVA
+    rownames(res) <- rownames(sigTable[[1]])
+  }
+  
+  return(res)
 }
