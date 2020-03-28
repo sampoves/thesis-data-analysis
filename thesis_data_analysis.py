ca1a167aa770fa69a840335895ec8427004923e6
Sampo Vesanen <31888573+sampoves@users.noreply.github.com>
2020-03-28 17:40:08 +0200

    better column calculation in postal
    added median. work continues

--- thesis_data_analysis.py
+++ thesis_data_analysis.py
@@ -12,9 +12,14 @@ to all packages delivered with Anaconda, GeoPandas 0.5.0 with its dependencies
 was installed for this work. Unfortunately I can't guarantee the smooth execution 
 of this code on newer package versions.
 
+Please note that this script talks about "respondents" and "IP addresses"
+interchangeably. For the most part an unique IP address can be considered to
+be a unique person, but this can't be verified as households and public venues
+share a wifi and therefore IP addresses. For simplicity this script assumes
+an unique IP address is an unique person.
+
+
 TODO, maybe
-    - Geographic analyses (Can't get geoplot to work)
-    - Respondent specific reports (probably not useful)
     - fix crs mismatches
     - user "oe4guro3dh", remove from visitors. Also, see what's going on with
     the discrepancy. the three records really do not exist in visitors
@@ -32,21 +37,27 @@ from rtree import index
 import random
 
 # Working directories
-wd = r"C:\Sampon\Maantiede\Master of the Universe\python"
-datawd = r"C:\Sampon\Maantiede\Master of the Universe\leaflet_survey_results"
-ttm_path = r"C:\Sampon\Maantiede\Master of the Universe\HelsinkiTravelTimeMatrix2018\{0}xxx\travel_times_to_ {1}.txt"
+wd = r"C:\Sampon\Maantiede\Master of the Universe"
+script_wd = r"C:\Sampon\Maantiede\Master of the Universe/python"
+ttm_path = r"HelsinkiTravelTimeMatrix2018\{0}xxx\travel_times_to_ {1}.txt"
 
 # Survey data file paths
-records_data = os.path.join(datawd, "records.csv")
-visitors_data = os.path.join(datawd, "visitors.csv")
+records_data = r"leaflet_survey_results\records.csv"
+visitors_data = r"leaflet_survey_results\visitors.csv"
 
-os.chdir(wd)
+# Spatial data paths
+grid_path = r"python\MetropAccess_YKR_grid_EurefFIN.shp"
+resarea_path = r"python\paavo\pno_dissolve.shp"
+postal_path = r"python\paavo\2019\pno_tilasto_2019.shp"
+ykr_path = r"yhdyskuntarakenteenvyoh17\YKRVyohykkeet2017.shp"
+ua_forest_path = r"python\FI001L3_HELSINKI\ua2012_research_area.shp"
 
 # Import functions and lists of zipcodes. Has to be done after inserting 
 # os.chdir()
+os.chdir(script_wd)
 from thesis_data_analysis_funcs import *
 from thesis_data_zipcodes import *
-
+os.chdir(wd)
 
 
 ###################
@@ -54,58 +65,50 @@ from thesis_data_zipcodes import *
 ###################
 
 # Survey data. Lambda x to preserve leading zeros
-records = pd.read_csv(records_data, converters={"zipcode": lambda x: str(x)})
-visitors = pd.read_csv(visitors_data)
+records = pd.read_csv(records_data, 
+                      converters={"zipcode": lambda x: str(x)}, 
+                      index_col=[0])
+visitors = pd.read_csv(visitors_data, index_col=[0])
 
 # Shapefiles
-grid = gpd.read_file("MetropAccess_YKR_grid_EurefFIN.shp", encoding="utf-8")
-resarea = gpd.read_file("paavo\pno_dissolve.shp", encoding="utf-8")
-postal = gpd.read_file(r"paavo\2019\pno_tilasto_2019.shp", encoding="utf-8")
+grid = gpd.read_file(grid_path, encoding="utf-8")
+resarea = gpd.read_file(resarea_path, encoding="utf-8")
+postal = gpd.read_file(postal_path, encoding="utf-8")
 
 # Import urban zones shapefile and select only relevant area for this study
-ykr_vyoh = gpd.read_file(
-        r"C:\Sampon\Maantiede\Master of the Universe\yhdyskuntarakenteenvyoh17\YKRVyohykkeet2017.shp", 
-        encoding="utf-8")
+ykr_vyoh = gpd.read_file(ykr_path, encoding="utf-8")
 ykr_vyoh = gpd.overlay(ykr_vyoh, 
                        gpd.GeoDataFrame(geometry=resarea.buffer(500)), 
                        how="intersection")
 
-# From Urban Atlas 2012 preserve only forests. Select only relevant area for
-# this study
-forest = gpd.read_file(r"FI001L3_HELSINKI\ua2012_research_area.shp", 
-                       encoding="utf-8")
+# From Urban Atlas 2012 Helsinki area preserve only forests
+forest = gpd.read_file(ua_forest_path, encoding="utf-8")
 forest = forest[forest["ITEM2012"] == "Forests"]
 
 
 
 ###########################
-### PREPARE SOURCE DATA ###
+### PROCESS SURVEY DATA ###
 ###########################
 
-# Remove column "Unnamed: 0"
-records = records.drop(columns=["Unnamed: 0"])
-visitors = visitors.drop(columns=["Unnamed: 0"])
-
-# Timestamps to datetime 
+# Timestamps to datetime format
 records["timestamp"] = convertToDatetime(records, "timestamp")
 visitors["ts_first"] = convertToDatetimeVar2(visitors, "ts_first")
 visitors["ts_latest"] = convertToDatetimeVar2(visitors, "ts_latest")
 
-# visitors, records: remove author's visits and three records inputted by
-# the author. I have visited the survey from university, work and mobile
-# phone and these will be impossible to trace down.
+# visitors, records: remove author's home visits and three records inputted by
+# the author. Additionally, I have visited the survey from the university, 
+# work, and with mobile phone. These visits are impossible to trace.
 records = records.drop([0, 5, 6])
-
-# I was contacted on 24.5.2019 22.41 that a respondent had filled three 
-# zipcodes erroneously: Kallio, Käpylä and one other (probably Pukinmäki-Savela).
-# The following removes those records.
-records = records.drop([1277, 1278, 1279])
-
-# Author's visits
 visitors = visitors.iloc[1:] 
 visitors = visitors.drop([3753])
 
-# Alert user if records has IP codes which do not exist in visitors.
+# I was contacted on 24.5.2019 22.41 with the message that a respondent had 
+# filled three responses erroneously: for Kallio, Käpylä and one other 
+# (probably Pukinmäki-Savela). Remove these records.
+records = records.drop([1277, 1278, 1279])
+
+# Alert if records has IP codes which do not exist in visitors
 diff = list(set(records.ip) - set(visitors.ip))
 if diff != 0:
     print("NB! The following IP codes exist only in records: {0}".format(
@@ -114,16 +117,18 @@ if diff != 0:
 
 
 
-##########################
-### PROCESS SHAPEFILES ###
-##########################
+######################
+### PROCESS POSTAL ###
+######################
 
 # Process Helsinki Capital Region GeoDataFrame from PAAVO data
 muns = ["091", "049", "092", "235"]
 postal = postal[postal.kunta.isin(muns)]
 postal = postal.reset_index().drop(columns=["index", "euref_x", "euref_y"])
 
-### Remove islands unreachable by car
+# Remove islands unreachable by car. This process would have been easy to do 
+# in QGIS, for example, but here we have the same process realised in an
+# overcomplicated way.
 
 # Preserve two largest Polygons from these postal code areas
 preserveTwoLargest = ["00830", "00570", "00340", "00200", "00890"]
@@ -209,8 +214,9 @@ for idx, geom in enumerate(postal.geometry):
 # My survey data and Travel Time Matrix 2018 operate in different spatial
 # units. TTM is available in YKR grid cells, while my data was gathered
 # with PAAVO zipcode delineations as the base unit. In order to compare TTM and 
-# my findings I need to add postal area code data to grid. This enables me to
-# assign each postal code area with mean data about parktime and walktime.
+# my findings I need to add postal area code data to the GeoDataFrame grid. 
+# This enables me to assign each postal code area with mean data about parktime 
+# and walktime.
             
 # The for loop below assigns zipcode to a cell when more than 50 percent of the
 # current cell area is in that zipcode. Helsinki Capital Region is of different
@@ -223,21 +229,20 @@ grid_areas = gpd.GeoDataFrame(columns=["YKR_ID", "zipcode", "largest_area",
 crs = grid.crs
 spatial_index = grid.sindex # GeoPandas spatial index
 
-# Use i to create new rows to grid_areas
+# Use i to create new rows in GeoDataFrame grid_areas
 i = 0
 for idx, postalarea in postal.iterrows():   
-    #print("now processing {0}".format(postalarea.nimi))
-    
+
     possible_matches_index = list(
             spatial_index.intersection(postalarea.geometry.bounds))
     possible_matches = grid.iloc[possible_matches_index]
     precise_matches = possible_matches[
             possible_matches.intersects(postalarea.geometry)]
     
-    # current zipcode as df
+    # Make the current zipcode to a GeoDataFrame
     thisZip = gpd.overlay(precise_matches,
-                        gpd.GeoDataFrame(crs=crs, geometry=[postalarea.geometry]),
-                        how="intersection")
+                          gpd.GeoDataFrame(crs=crs, geometry=[postalarea.geometry]),
+                          how="intersection")
     thisZip["area1"] = thisZip.area
 
     # Iterate through all rows in current zipcode and see if each YKR_ID is
@@ -269,21 +274,22 @@ for idx, postalarea in postal.iterrows():
             grid_areas.loc[found_idx, "total_area"] += row.area1
 
 
-# Find out which grid ykr-ids do not occur in grid_areas. These cells are 
-# outside of PAAVO postal code areas
+# Find out which grid ykr ids do not occur in GeoDataFrame grid_areas. These 
+# cells are outside of PAAVO postal code area boundaries
 notPresent = list(np.setdiff1d(list(grid.YKR_ID), list(grid_areas.YKR_ID)))
 
-# Add cells to grid_areas
+# Add these cells in GeoDataFrame grid_areas as zipcode 99999
 for cell_id in notPresent:
     grid_areas = grid_areas.append(
         {"YKR_ID": np.int64(cell_id), 
          "zipcode": "99999"}, 
         ignore_index=True)
 
-# Incorporate grid_areas area data to grid
+# Incorporate "grid_areas" to the original GeoDataFrame grid
 grid = pd.concat(
         [grid.set_index("YKR_ID"), grid_areas.set_index("YKR_ID")], 
-        axis=1, join="inner").reset_index()
+        axis=1, 
+        join="inner").reset_index()
 
 
 
@@ -291,10 +297,10 @@ grid = pd.concat(
 ### RESPONDENT BEHAVIOUR ###
 ############################
 
-# Create groupby aggregations of the records. One can use this dataframe to
-# see how each respondent answered to the survey. This feature is not developed
-# any further from here.
-visitor_grp = records.groupby(["ip"]).agg({
+# Create groupby aggregations of the records. One can use this DataFrame to
+# see how each respondent (or, specifically, one IP address) answered to the 
+# survey.
+visitors_grp = records.groupby(["ip"]).agg({
         "id": "count", 
         "timestamp": lambda x: x.tolist(),
         "zipcode": lambda x: x.tolist(),
@@ -303,24 +309,26 @@ visitor_grp = records.groupby(["ip"]).agg({
         "parktime": lambda x: x.tolist(),
         "walktime": lambda x: x.tolist(),
         "timeofday": lambda x: x.tolist()})
-visitor_grp = visitor_grp.rename(columns={"id": "amount"})
-    
+visitors_grp = visitors_grp.rename(columns={"id": "amount"})
+#visitors_grp.to_excel("visitors_grouped.xlsx")
         
+
 
 #################################
 ### DETECTION OF ILLEGAL DATA ###
 #################################
 
 # Erase contents of duplicates.txt before writing anything (wipe old data). 
-# Wrap the following for loop in with loop. This enables us to write the 
-# duplicates report as text file in the working directory for easy viewing.
-# Edit Pandas max_colwidth to show even the lengthiest rows, default 50.
+# Wrap the following for loop in a "with" loop. This enables us to write the 
+# duplicates report as a text file in the working directory for easy viewing.
+# Edit Pandas max_colwidth to show even the lengthiest rows. Pandas default is 
+# 50.
 pd.options.display.max_colwidth = 100
-open(os.path.join(wd, "duplicates.txt"), "w").close()
+open("duplicates.txt", "w").close()
 
-# Detect if a user has answered a same area multiple times. Please note that 
-# this only locates duplicates but does nothing about it. I will leave the
-# discussion about this to the thesis.
+# Detect if a respondent has answered to the same area multiple times. Please 
+# note that this only locates duplicates but does nothing about it. I will leave 
+# the discussion about this to the thesis.
 with open(os.path.join(wd, "duplicates.txt"), "a+") as f:
     for visitor in records.ip.unique():
         theseRecords = records.loc[records['ip'] == visitor]
@@ -351,7 +359,7 @@ print("\nA report on duplicate answers saved to disk in path {0}\n".format(
 # Remove illegal answers. At this time any value equal or above 60 minutes is 
 # deemed illegal. Value invalid starts at 6 as at this point I have removed 3 
 # of my own records and 3 others which were reported to me as false. See above,
-# part "Prepare source data"
+# part "Process survey data"
 delList = []
 illegal = []
 invalid = 6
@@ -367,7 +375,7 @@ for idx, (park_value, walk_value, ip_value) in enumerate(
         invalid += 1
  
 # Illegal contains all IPs which have values equal or above 60 as answers. 
-# illegal_df shows all records made by these IP addresses
+# DataFrame "illegal_df" shows all records made by these IP addresses
 illegal = list(set(illegal))
 illegal_df = records[records["ip"].isin(illegal)]
 
@@ -397,7 +405,7 @@ ykr_vyoh["vyohselite"] = ykr_vyoh["vyohselite"].replace(
          "alakeskuksen jalankulkuvyöhyke/joukkoliikenne": 
              "alakeskuksen jalankulkuvyöhyke"})
 
-# Add ykr_vyoh columns
+# Add additional columns
 postal["ykr_kesk_jalan"] = 0
 postal["ykr_kesk_reuna"] = 0
 postal["ykr_int_joukko"] = 0
@@ -405,39 +413,22 @@ postal["ykr_joukkoliik"] = 0
 postal["ykr_alakesk_jalan"] = 0
 postal["ykr_autovyoh"] = 0
 postal["ykr_novalue"] = 0
-
-# Add Urban Atlas 2012 forest amount
 postal["ua_forest"] = 0
 
-# Add column "answer count", "parktime mean", and "walktime mean"
-postal["answer_count"] = 0
-postal["parktime_mean"] = 0
-postal["walktime_mean"] = 0
+# Calculate column "answer_count". Answer count for each zipcode area
+answ = records.zipcode.value_counts().rename("answer_count")
+postal = postal.join(answ, on="posti_alue")
 
-# Calculate answer count for each zipcode area
-for zipcode in records.zipcode:
-    for idx, postal_zip in enumerate(postal.posti_alue):
-        if postal_zip == zipcode:
-            postal.loc[idx, "answer_count"] += 1
-
-# Calculate parktime mean to postal.parktime_mean
-for idx, postal_zip in enumerate(postal.posti_alue):
-    this_mean = records.loc[
-            records['zipcode'] == postal_zip]["parktime"].mean()
-    postal.loc[idx, "parktime_mean"] = round(this_mean, 2)
-
-# Calculate walktime mean to postal.walktime_mean
-for idx, postal_zip in enumerate(postal.posti_alue):
-    this_mean = records.loc[
-            records['zipcode'] == postal_zip]["walktime"].mean()
-    postal.loc[idx, "walktime_mean"] = round(this_mean, 2)
+# Calculate mean and median for parktime and walktime. Join by zipcode
+roundmean = lambda x: round(x.mean(), 2)
+calcs = records.groupby("zipcode").agg(
+        {"parktime": {"Mean": roundmean, "Median": "median"},
+         "walktime": {"Mean": roundmean, "Median": "median"}})
+calcs.columns = "parktime_mean", "parktime_median", "walktime_mean", "walktime_median"
+postal = postal.join(calcs, on="posti_alue")
 
 # Prepare annotation of Polygon features
 postal["coords"] = polygonCoordsToTuple(postal)
-
-# Population density
-#postal["pop_dens"] = postal.apply(
-#        lambda row: round(row["he_vakiy"]/(row["pinta_ala"]/1000000), 2), axis=1)
 
 
 
@@ -692,7 +683,8 @@ for varname, fullname in subdiv_dict.items():
 # Use Corine Land Cover 2018 shapefile to find the most common land cover type
 # in level 3 for each zipcode.
             
-# Do not utilise this for the time being
+# Do not utilise this for the time being. There are significant problems with
+# trying to put the levels into my data.
 
 ## Import Corine Land Cover 2018, select only relevant area
 #clc2018 = gpd.read_file(
@@ -804,20 +796,28 @@ firstfile.to_csv(ttm_sampo + firstfilename, sep=";", index=None)
 
 
 
-#################
-### VISUALISE ###
-#################
+############################
+### VISUALISE & DESCRIBE ###
+############################
 # Needs more advanced plotting. Compare to other data I have available,
 # population, area etc
 #https://towardsdatascience.com/lets-make-a-map-using-geopandas-pandas-and-matplotlib-to-make-a-chloropleth-map-dddc31c1983d
 
 # PLOT AMOUNT OF RECORDS
 # Plot with layers as function
-parkingPlot(postal, "answer_count", 0) #AMOUNT OF RECORDS
+parkingPlot(postal, "answer_count", 0) 
+parkingPlot(postal[postal["kunta"] == "091"], "answer_count", 0) #amount for Hki
 parkingPlot(postal, "walktime_mean", 1) #PLOT WALKTIME MEAN
 parkingPlot(postal, "parktime_mean", 1) #PLOT PARKTIME MEAN
+parkingPlot(postal, "ua_forest", 1) #PLOT PARKTIME MEAN
 
-
+# Get mean-std-min-max-quantiles of municipalities
+descri_postal = postal.iloc[:, [4, 113, 114, 115, 116, 117]]
+descri_postal.describe()
+descri_postal[postal["kunta"] == "091"].describe() #hki
+descri_postal[postal["kunta"] == "092"].describe() #esp
+descri_postal[postal["kunta"] == "235"].describe() #kau
+descri_postal[postal["kunta"] == "049"].describe() #van
 
 #####################
 ### EXPORT TO CSV ###
@@ -826,3 +826,14 @@ parkingPlot(postal, "parktime_mean", 1) #PLOT PARKTIME MEAN
 # data to csv for R. "pythonrecords.csv"
 #records.to_csv(wd + "records.csv", encoding="Windows-1252")
 #postal.to_csv(wd + "postal.csv", encoding="Windows-1252")
+
+# For Shinyapps.io. Huge problems with character encoding down the line if
+# Ascii-UTF-8 conversion is not done here.
+#import unicodedata
+#records["subdiv"] = records["subdiv"].apply(lambda val: unicodedata.normalize("NFKD", val).encode("ascii", "ignore").decode())
+#records["ykr_zone"] = records["ykr_zone"].apply(lambda val: unicodedata.normalize("NFKD", val).encode("ascii", "ignore").decode())
+#records.to_csv(wd + "/shinyrecords.csv", encoding="utf-8")
+
+#postal["nimi"] = postal["nimi"].apply(lambda val: unicodedata.normalize("NFKD", val).encode("ascii", "ignore").decode())
+#postal["namn"] = postal["namn"].apply(lambda val: unicodedata.normalize("NFKD", val).encode("ascii", "ignore").decode())
+#postal.to_csv(wd + "/shinypostal.csv", encoding="utf-8")
